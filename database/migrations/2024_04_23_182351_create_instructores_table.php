0699fbaef477b19078486529ea3b3d798cfa671d
Kevin Julian <kevin.n2001@hotmail.com>
2024-04-23 13:40:07 -0500

    Migraciones

---
+++ database/migrations/2024_04_23_182351_create_instructores_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('instructores', function (Blueprint $table) {
+            $table->id('id_instructores')->unique();
+            $table->string('nombre', 64)->nullable();
+            $table->string('apellido', 64)->nullable();
+            $table->string('especialidad');
+            $table->string('biografía');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('instructores');
+    }
+};
---
+++ database/migrations/2024_04_23_182729_create_inscripciones_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('inscripciones', function (Blueprint $table) {
+            $table->id('id_inscripciones')->unique();
+            $table->integer('id_cursos');
+            $table->integer('id_instructores');
+            $table->integer('id_estudiantes');
+            $table->date('fecha_inscripcion');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('inscripciones');
+    }
+};
