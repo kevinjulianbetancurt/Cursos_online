92f7dead0afe8b1060014d627b63463cb35a75d4
Kevin Julian <kevin.n2001@hotmail.com>
2024-04-23 15:26:08 -0500

    Vista(Index-Cursos)

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CursosController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/cursos', [CursosController::class, 'index']);
